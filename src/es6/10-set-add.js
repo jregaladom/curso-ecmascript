dfaa81dc218d3e294f3541ea387a4fb462d478e5
Jose Juan Regalado <jregaladomoncada@gmail.com>
2023-03-27 07:54:09 -0600

    Avance curso

---
+++ src/es6/10-set-add.js
@@ -0,0 +1,8 @@
+const list = new Set();
+list.add(5);
+list.add(7);
+list.add(6);
+list.add(5);
+
+
+console.log(list);
---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,7 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers.includes(7));
+console.log(numbers.includes(70));
+
+const nanmes = ['Juan', 'Pedro', 'Luis', 'Maria', 'Jose'];
+console.log(nanmes.includes('Jose'));
